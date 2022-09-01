e9f937a368160fb01c3379080a6aac0584a2922a
Shako Kinyamba <shakokinyamba201@gmail.com>
2022-09-01 10:37:06 +0200

    add card

---
+++ .history/src/components/Leading/Leading_20220901084005.jsx
@@ -0,0 +1,52 @@
+import React ,{useContext,useEffect,useState} from 'react';
+import Navbar from '../Header/Navbar';
+import {StateContext} from '../context/ContextProfil';
+import Sidbar from '../Sidebar/Sidbar';
+
+
+
+const Leading = () => {
+
+    const activeLink ="flex items-center gap-5 pl-4 pt-3 pb-2.5 rounded-lg text-white text-md m-2";
+    const normalLink ="flex items-center gap-5 pl-4 pt-3 pb-2.5 rounded-lg  text-md text-gray-700 dark:text-gray-200 dark:hover:text-black hover:bg-light-gray m-2";
+    const {tokens,setTokens,music,setMusics}=useContext(StateContext)
+    const [searchKey, setSearchKey] = useState("a")
+    const [artists, setArtists] = useState([])
+
+    useEffect(() => {
+          setTokens(window.localStorage.getItem('token'))
+    }, [])
+
+    const searchArtists = async (e) => {
+        e.preventDefault()
+        console.log(tokens);
+        fetch(`https://api.spotify.com/v1/search?q=${searchKey}&type=artist`,
+        {headers: {'Authorization':`Bearer ${tokens}`},"Content-Type":"application/json"
+    })
+        .then((reponse)=>{
+            if(reponse.status===200){
+                return reponse.json()
+            }
+        })
+        .then((reponse))
+
+
+        
+    }
+    
+    
+
+    return (
+        <>
+            <Navbar search={searchArtists}/>
+            <div className='flex justify-between'>
+               <Sidbar/>
+               <div></div>
+            </div>
+           
+        </>
+        
+    );
+};
+
+export default Leading;
---
+++ .history/src/components/Sidebar/Sidbar_20220831215245.jsx
@@ -0,0 +1,34 @@
+import React, { useContext,useState} from 'react'
+import { StateContext } from '../context/ContextProfil'
+import {AiOutlineLeft} from 'react-icons/ai'
+import { CgMenuGridR } from 'react-icons/cg'
+import { menu } from '../Data/Data'
+
+
+const Sidbar = () => {
+  const [open,setOpen]=useState(true)
+  const { activeMenu, setActiveMenu, screensize } = useContext(StateContext)
+  return (
+    <div className="flex">
+        <div className={` ${open ? 'w-72':'w-20'} duration-300 bg-primary h-screen mt-20 relative flex flex-col gap-7`}>
+            
+            <AiOutlineLeft size={40} className={`text-primary bg-gray-100  rounded-full
+              absolute cursor-pointer right-1 top-9 w-7 h-7 border-5 border-primary
+              ${!open && "rotate-180"}
+            `}
+            onClick={()=>{setOpen(!open)}}
+            />
+
+            {menu.map((item, index) => (
+                        <div className="flex gap-5 ml-5 mr-5 rounded mt-18 space-x-2 " key={index}>
+                        {<span className={`text-5xl text-gray-200 ${open &&"rotate-[360deg] hover:scale-105"}`}>{item.icon}</span>}
+                        <button className={`text-ColorText text-2xl font-semibold ${!open && "scale-0 duration-300"}`}>{item.name}</button>
+                        </div>
+                    ))}
+        </div>
+    </div>
+   
+  )
+}
+
+export default Sidbar
